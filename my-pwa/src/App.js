a6a0c7f5664893a042e6952c29297acc75b28f44
Shreyas Deshpande <161595169+shrey02802@users.noreply.github.com>
2025-08-23 17:43:07 +0530

    app

--- my-pwa/src/App.js
+++ my-pwa/src/App.js
@@ -1,18 +1,8 @@
-import React from "react";
-import "./App.css";
-import generatedImage from "./generated-image.png"; // your image
-
 function App() {
   return (
-    <div className="App" style={{ textAlign: "center", marginTop: "50px" }}>
-      <img 
-        src={generatedImage} 
-        alt="PWA Preview" 
-        style={{ maxWidth: "80%" }} 
-      />
-    </div>
+    <h1 style={{ textAlign: "center", marginTop: "50px" }}>
+      ✅ Successful Deploy on Amplify!
+    </h1>
   );
 }
-
-export default App;
 
