f4457c75905223bc0087dc4953d6e1475e77d965
Shreyas Deshpande <161595169+shrey02802@users.noreply.github.com>
2025-08-23 17:11:46 +0530

    updated appjs and image

--- my-pwa/src/App.js
+++ my-pwa/src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import generatedImage from "./generated-image.png"; // your image
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ textAlign: "center", marginTop: "50px" }}>
+      <img 
+        src={generatedImage} 
+        alt="PWA Preview" 
+        style={{ maxWidth: "80%" }} 
+      />
     </div>
   );
 }
 
 export default App;
+
